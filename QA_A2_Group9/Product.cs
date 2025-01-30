536afa3bc4a111771d6704f50743300b89889fa7
Aline Sathler <sathler@ymail.com>
2025-01-29 19:54:57 -0500

    Initial commit - Project setup

---
+++ QA_A2_Group9/Product.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_A2_Group9
+{
+    public class Product
+    {
+        #region Properties
+        int productId;
+        string productName;
+        decimal price;
+        int quantity;
+        #endregion
+
+        #region Constructors
+        public Product(int productId, string productName, decimal price,int quantity)
+        {
+            // Check if the product ID is within the range of 5 to 50000
+            if (productId < 5 || productId > 50000) {
+                throw new ArgumentOutOfRangeException("", "ProductId is out of the range");
+            }
+
+            // Check if the product name is null or empty
+            if (string.IsNullOrWhiteSpace(productName)) {
+                throw new ArgumentNullException("", "Product Name is null or empty");
+            }
+
+            // Check if the price is within the range of 5 to 5000
+            if (price < 5 || price > 5000) {
+                throw new ArgumentOutOfRangeException("", "Price is out of the range");
+            }
+
+            // Check if the quantity is within the range of 5 to 500000
+            if (quantity < 5 || quantity > 500000) {
+                throw new ArgumentOutOfRangeException("", "Quantity is out of the range");
+            }
+
+            this.productId = productId;
+            this.productName = productName;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Output the product details
+        /// </summary>
+        /// <returns>String with product details</returns>
+        public override string ToString() { return $"Product ID: {this.productId}, Name: {this.productName}, Price: {this.price.ToString("C")}, Quantity: {this.quantity}"; }
+
+        /// <summary>
+        /// Increment the quantity of the product by qty
+        /// </summary>
+        /// <param name="qty"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void IncreaseStock(int qty) {
+            if (qty < 0) {
+                throw new ArgumentOutOfRangeException("", "Increment must be positive.");
+            }
+
+            if (this.quantity + qty > 500000) {
+                throw new ArgumentOutOfRangeException("", "Quantity is out of the range");
+            }
+
+            this.quantity += qty;
+        }
+
+        /// <summary>
+        /// Decrement the quantity of the product by qty
+        /// </summary>
+        /// <param name="qty"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void DecreaseStock(int qty) {
+            if (qty < 0) {
+                throw new ArgumentOutOfRangeException("", "Decrement must be positive.");
+            }
+            if (this.quantity - qty < 5) {
+                throw new ArgumentOutOfRangeException("", "Quantity is out of the range");
+            }
+
+            this.quantity -= qty;
+        }
+        #endregion
+    }
+}
---
+++ QA_A2_Group9/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace QA_A2_Group9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Products");
+
+            // Instantiate a product object
+            Product product1 = null;
+
+            // Create a product with valid values
+            try {
+                product1 = new Product(5, "Books", 20.00m, 5);
+                Console.WriteLine(product1.ToString());
+
+                // Increment the quantity of the product by 5
+                try {
+                    product1.IncreaseStock(5);
+                    Console.WriteLine(product1.ToString());
+                } catch (Exception ex) {
+                    Console.WriteLine($"Error increasing stock: {ex.Message}");
+                }
+
+                // Decrement the quantity of the product by 5
+                try {
+                    product1.DecreaseStock(5);
+                    Console.WriteLine(product1.ToString());
+                } catch (Exception ex) {
+                    Console.WriteLine($"Error decreasing stock: {ex.Message}");
+                }
+            } catch (Exception ex) {
+                Console.WriteLine($"Error creating product: {ex.Message}");
+            }
+
+
+        }
+    }
+}
